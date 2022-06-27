9b61cec5052afb50de0c7977517577f7b0fd1ff4
joiplay <joiplay@protonmail.com>
2022-06-27 11:58:40 +0300

    Fix a compatibility issue with 7.3.5

--- renpy/python.py
+++ renpy/python.py
@@ -1057,6 +1057,8 @@ def py_exec_bytecode(bytecode, hide=False, globals=None, locals=None, store="sto
 
     if locals is None:
         locals = globals # @ReservedAssignment
+    
+    locals["store"] = renpy.store.store
 
     exec(bytecode, globals, locals)
 
@@ -1070,6 +1072,8 @@ def py_exec(source, hide=False, store=None):
         locals = { } # @ReservedAssignment
     else:
         locals = store # @ReservedAssignment
+    
+    locals["store"] = renpy.store.store
 
     exec(py_compile(source, 'exec'), store, locals)
 
@@ -1081,6 +1085,8 @@ def py_eval_bytecode(bytecode, globals=None, locals=None): # @ReservedAssignment
 
     if locals is None:
         locals = globals # @ReservedAssignment
+    
+    locals["store"] = renpy.store.store
 
     return eval(bytecode, globals, locals)
 
