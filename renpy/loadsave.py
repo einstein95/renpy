d65c73721bc0e8d6144a622fefb54808c8310f61
joiplay <joiplay@protonmail.com>
2022-08-16 13:08:27 +0300

    Import pickle and renpy.compat.pickle.dumps to keep compatibility with old versions.

--- renpy/loadsave.py
+++ renpy/loadsave.py
@@ -40,7 +40,8 @@ import sys
 import renpy
 from json import dumps as json_dumps
 
-from renpy.compat.pickle import dump, loads
+import pickle
+from renpy.compat.pickle import dump, dumps, loads
 
 
 # This is used as a quick and dirty way of versioning savegame
