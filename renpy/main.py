a744fc359de2dceb507d40c8f8f3cd0b1c9514af
joiplay <joiplay@protonmail.com>
2022-09-29 15:46:17 +0300

    Prevent crash when patches.json doesn't exist

--- renpy/main.py
+++ renpy/main.py
@@ -385,10 +385,13 @@ def main():
     if "RENPY_PATCHPATH" in os.environ:
         import json
 
-        with open(os.environ["RENPY_PATCHPATH"], 'r', encoding= 'utf-8') as pf:
-            patchdata = pf.read()
-            renpy.config.patch_list = json.loads(patchdata)['renpy']
-            print("Loaded "+str(len(renpy.config.patch_list))+" patches")
+        try:
+            with open(os.environ["RENPY_PATCHPATH"], 'r', encoding= 'utf-8') as pf:
+                patchdata = pf.read()
+                renpy.config.patch_list = json.loads(patchdata)['renpy']
+                print("Loaded "+str(len(renpy.config.patch_list))+" patches")
+        except:
+            print("Could not read patches.")
 
     # Load Ren'Py extensions.
     for dir in renpy.config.searchpath: # @ReservedAssignment
